f8410ee814b68e9b9ac7ad70ce51f1659256febe
Michelle Mistry <mistrymichelle@gmail.com>
2021-04-21 14:17:17 +0100

    css and html changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from application import app
+# from application import app
+from flask import Flask
 
-
-
-if __name__== "__main__":
-    app.run(debug=True)
+#
+# app = Flask(__name__)
+#
+# if __name__== "__main__":
+#     app.run(debug=True)
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,20 @@
-from flask import render_template,request
+from flask import Flask, render_template, request, url_for
 from application import app
 
+app = Flask(__name__)
+
+# @app.route('/')
+# # @app.route('/home')
+# def layout():
+#     return render_template('layout.html', name='layout')
+
 @app.route('/')
-@app.route('/home')
+# @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', name='Home')
+
+
+
+
+if __name__== "__main__":
+    app.run(debug=True)
