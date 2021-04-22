667e147507fb3d946aa2036917d6b7a95035de8b
Tasnim <Tasnim.b98@gmail.com>
2021-04-22 14:06:21 +0100

    created login page and the tables

--- application/forms.py
+++ application/forms.py
@@ -1,2 +1,30 @@
-from flask import render_template,request
-from application import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+gender = ('Female', 'Male')
+
+class RegistrationForm(FlaskForm):
+    title = StringField('title',validators=[DataRequired(), Length(min=2, max=10)])
+    first_name = StringField('firstname',validators=[DataRequired(), Length(min=2, max=20)])
+    last_name = StringField('lastname', validators=[DataRequired(), Length(min=2, max=20)])
+    dob = IntegerField('dob', validators=[DataRequired(), Length(min=8, max=10)])
+    email = StringField('Email',validators=[DataRequired(), Length(min=6, max=35)])
+    contact_number = IntegerField('phone',validators=[DataRequired(), Length(min=4, max=25)]) # check out regex
+    Address = StringField('Address',validators=[DataRequired(), Length(min=5, max=50)])
+    post_code = StringField('postcode', validators=[DataRequired(), Length(min=8, max=10)])
+    city = StringField('city', validators=[DataRequired(), Length(min=2, max=20)])
+    account_number = IntegerField('accountnumber',validators=[DataRequired(), Length(min=8, max=8)])
+    sort_code = IntegerField('sortcode',validators=[DataRequired(), Length(min=8, max=8)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    Login = SubmitField('Login')
+
--- application/routes.py
+++ application/routes.py
@@ -1,17 +1,54 @@
 from flask import Flask, render_template, request, url_for
-from application import app
+from application.forms import LoginForm
+# from application import app
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
-# @app.route('/')
-# # @app.route('/home')
-# def layout():
-#     return render_template('layout.html', name='layout')
 
 @app.route('/')
 # @app.route('/home')
 def home():
-    return render_template('home.html', name='Home')
+    return render_template('home.html', title='Home')
+
+@app.route('/book')
+def book():
+    return render_template('booking.html', title='Book')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
+
+@app.route('/policies')
+def policies():
+    return render_template('policies.html', title='policies')
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    error = ""
+    form = LoginForm()
+    if request.method == "POST" and form.validate():
+        email = form.email.data
+        password = form.password.data
+
+        if len(email) == 0 or len(password) == 0:
+            error = "Please enter your email address and password"
+        else:
+            return "You are now logged in "
+    return render_template("login.html", title="login", form=form, message=error )
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(CustomerLogin)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 
 
