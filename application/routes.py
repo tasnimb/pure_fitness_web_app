990474a05b70975e0d84e7b5766e0cc88b01e754
DumisileM <dumisilem@aol.com>
2021-04-29 14:15:37 +0100

    cleaning routes

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, session
-from datetime import timedelta
+
 from application.forms import LoginForm, RegistrationForm, BookActivity, Password_Reset
 from application import app, db, flask_change_password
 from application.models import CustomerContact, CustomerLogin, Activity, ActivityBook
-from flask_change_password import  ChangePassword, ChangePasswordForm, SetPasswordForm
-from datetime import date
-# import bcrypt
+
+
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -39,21 +39,15 @@ def login():
     if request.method == "POST" and form.validate():
         email = request.form.get('email')
         password = request.form.get('password')
-        # email = form.email.data
-        # password = form.password.data
 
-        user = CustomerLogin.query.filter_by(email_address=email).first() #telling the data base to filter on email addresses, to find the one the customer sends to the database
-        if not user or not check_password_hash(user.password, password): #this line here is only = True, if it = False then would redirect to dashboard, why is that?
-            flash("Please check your login details or sign-up for an account below.")  # add code in html for flash
-            # return redirect(url_for('register'))
+
+        user = CustomerLogin.query.filter_by(email_address=email).first()
+        if not user or not check_password_hash(user.password, password):
+            flash("Please check your login details or sign-up for an account below.")
+
         else:
-            return redirect(url_for('dashboard')) # not working
+            return redirect(url_for('dashboard'))
 
-        # session.permanent = True
-        # flash("Your session has timed out. You will need to sign back in.")
-
-        # if len(email) == 0 or len(password) == 0:
-        #     error = "Please enter your email address and password"
     return render_template("login.html", title="login", form=form, error=flash)
 
 
@@ -63,19 +57,6 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('home'))
 
-@app.route('/change_password', methods=['GET','POST'])
-def change_password():
-    form=ChangePasswordForm(username='email_address.CustomerLogin',title='Change Password')
-
-    if form.validate_on_submit():
-        valid = flask_change_password.verify_password_change_form(form)
-        if valid:
-            return redirect(url_for('login', title='login', new_password=form.password.data))
-
-
-    return render_template('change_password.html', password_template=password_template, title='Change password', form=form,
-                           user=dict(username='test.user'),
-                           )
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -91,20 +72,19 @@ def register():
         postcode = form.post_code.data
         city = form.city.data
         password = form.password.data
-        # .encode("utf-8") #unicode object for encoding password
-        # hashed = bcrypt.hashpw(password, bcrypt.gensalt()) # encrypt the password and assigning it to variable hashed
         details = CustomerContact(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth, phone_number=phone_number, address_line=address_line, postcode=postcode, city=city,)
-        login_d = CustomerLogin(email_address=email_address, password=generate_password_hash(password, method='sha256')) #this encryption solution takes up less memory as its just an import
+        login_d = CustomerLogin(email_address=email_address, password=generate_password_hash(password, method='sha256'))
         db.session.add(details)
         db.session.add(login_d)
         db.session.commit()
-        flash('Thanks for registering') #add code in html for flash
+        flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('register.html', error=flash, form=form)
 
 
 @app.route('/dashboard')
 def dashboard():
+
     return render_template('dashboard.html', title='dashboard')
 
 
